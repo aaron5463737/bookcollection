964dd531f1c3b261d7ddd47a1077c70d051abd5f
aaron54637 <aaron5463737@gmail.com>
2022-09-15 20:11:22 -0500

    tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -22,17 +22,6 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('J.K. Rowling')
   end
 
-  scenario 'valid date' do 
-    visit new_book_path
-    fill_in 'Title', with: 'Harry Potter'
-    fill_in 'Author', with: 'J.K. Rowling'
-    fill_in 'Price', with: 25.98
-    #fill_in 'published', with: "2001-03-13 00:00:00 UTC"
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('2001')
-  end
-
   scenario 'valid price' do 
     visit new_book_path
     fill_in 'Title', with: 'Harry Potter'
@@ -43,4 +32,14 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content(25.48)
   end
 
+  scenario 'valid price2' do 
+    visit new_book_path
+    fill_in 'Title', with: 'Harry Potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Price', with: 25.48
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(25.48)
+  end
+  
 end
